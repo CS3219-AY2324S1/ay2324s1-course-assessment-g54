85be4cc05fae4a9636fffcb358c281cf245630d7
Bryan Loh <bryanlohxz@gmail.com>
2023-09-06 22:58:58 +0800

    Add login and signup pages (#9)
    * Add basic login page
    
    * Add sign up page
    
    * Add check for confirm password to signup page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,8 +1,60 @@
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>This is the login page</h1>
-    </div>
+    <Box
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Card sx={{ padding: 3 }}>
+        <CardContent>
+          <Stack spacing={6}>
+            <Typography variant="h5" color="grey">
+              Welcome back, please login to your account.
+            </Typography>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+              }}
+            >
+              <Stack spacing={3}>
+                <TextField
+                  label="Email"
+                  variant="outlined"
+                  type="email"
+                  required
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <TextField
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  required
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+                <Button variant="contained" type="submit">
+                  Login
+                </Button>
+              </Stack>
+            </form>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
   );
 };
 
