e20565104f37dd71afc7a540e60dffe1c1688f24
Bryan Loh <bryanlohxz@gmail.com>
2023-09-06 17:48:23 +0800

    Add signup and profile endpoints

--- users/index.js
+++ users/index.js
@@ -7,12 +7,20 @@ app.post("/login", (req, res) => {
   res.send("Hello from the login endpoint!");
 });
 
+app.post("/logout", (req, res) => {
+  res.send("Hello from the logout endpoint!");
+});
+
+app.get("/profile", (req, res) => {
+  res.send("Hello from the profile endpoint!");
+});
+
 app.post("/profile", (req, res) => {
   res.send("Hello from the profile endpoint!");
 });
 
-app.post("/logout", (req, res) => {
-  res.send("Hello from the logout endpoint!");
+app.post("/signup", (req, res) => {
+  res.send("Hello from the login endpoint!");
 });
 
 app.listen(PORT, () => {
