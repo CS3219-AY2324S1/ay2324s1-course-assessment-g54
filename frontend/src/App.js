8c1023b54bcbba530f6905f897643e65ae2808df
Bryan Loh <bryanlohxz@gmail.com>
2023-09-06 17:00:35 +0800

    Set up frontend with material ui react

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Example from "./pages/Example";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Questions from "./pages/Questions";
+import Signup from "./pages/Signup";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+const theme = createTheme();
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Example />} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/auth/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
