bb6db6e98816182122a09f6b224d3572cefd3a39
Bryan Loh <bryanlohxz@gmail.com>
2023-09-06 17:28:24 +0800

    Rename authentication to users microservice

---
+++ users/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+app.post("/login", (req, res) => {
+  res.send("Hello from the login endpoint!");
+});
+
+app.post("/profile", (req, res) => {
+  res.send("Hello from the profile endpoint!");
+});
+
+app.post("/logout", (req, res) => {
+  res.send("Hello from the logout endpoint!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Authentication app listening on port ${PORT}`);
+});
