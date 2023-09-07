5bfa1e565fa6190ce635941519fbc430ad01fe80
Bryan Loh <bryanlohxz@gmail.com>
2023-09-08 04:04:11 +0800

    Set up database migrations for users service (#16)
    * Add knex migrations to users service
    
    * Update README to include users database migrations

--- users/migrations/0001-create-user-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS citext;
-
-CREATE TABLE users (
-  id              UUID    PRIMARY KEY   DEFAULT gen_random_uuid(),
-  name            TEXT    NOT NULL,
-  email           TEXT    NOT NULL      UNIQUE,
-  passwordHash    TEXT    NOT NULL
-);
-
