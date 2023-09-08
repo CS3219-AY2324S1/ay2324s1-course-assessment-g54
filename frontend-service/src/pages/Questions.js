f9bb3b87c64b168fecea1e1cebc9099678fae3ae
Bryan Loh <bryanlohxz@gmail.com>
2023-09-08 17:43:56 +0800

    Edit navbar and fix some authguard bugs

--- frontend-service/src/pages/Questions.js
+++ frontend-service/src/pages/Questions.js
@@ -1,65 +1,70 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import NavBar from './components/NavBar';
-import Typography from '@mui/material/Typography';
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+import NavBar from "./components/NavBar";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 
 const Questions = () => {
-
   const [questions, setQuestions] = useState([]);
-  
-  useEffect(() => {
-        // Define a function to fetch data from the API
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(
-              "http://localhost:3001/api/question/getQuestions"
-            );
-    
-            if (response.status === 200) {
-              setQuestions(response.data);
-            } else {
-              console.error("Failed to fetch questions.");
-            }
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        };
-    
-        // Call the fetchData function when the component mounts
-        fetchData();
-      }, []); // The empty dependency array means this effect will run once when the component mounts
 
+  useEffect(() => {
+    // Define a function to fetch data from the API
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3001/api/question/getQuestions"
+        );
+
+        if (response.status === 200) {
+          setQuestions(response.data);
+        } else {
+          console.error("Failed to fetch questions.");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    // Call the fetchData function when the component mounts
+    fetchData();
+  }, []); // The empty dependency array means this effect will run once when the component mounts
 
   return (
     <div>
-      <NavBar/>
-      <Typography variant="h3" color="initial">Questions</Typography>
+      <NavBar />
+      <Typography variant="h3" color="initial">
+        Questions
+      </Typography>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell >Title</TableCell>
-              <TableCell >Category</TableCell>
-              <TableCell >Complexity</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Category</TableCell>
+              <TableCell>Complexity</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {questions.map((question) => (
               <TableRow
                 key={question.question_id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
                   {question.question_title}
                 </TableCell>
-                <TableCell>{question.question_categories.map((cat) => (<div>{cat}</div>))}</TableCell>
+                <TableCell>
+                  {question.question_categories.map((cat) => (
+                    <div>{cat}</div>
+                  ))}
+                </TableCell>
                 <TableCell>{question.question_complexity}</TableCell>
               </TableRow>
             ))}
--- frontend-service/src/pages/components/NavBar.js
+++ frontend-service/src/pages/components/NavBar.js
@@ -1,161 +1,81 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import { useUser } from "../../contexts/UserContext";
+
+import AppBar from "@mui/material/AppBar";
+import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Popover from "@mui/material/Popover";
+import Stack from "@mui/material/Stack";
+import Toolbar from "@mui/material/Toolbar";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
 
 function NavBar() {
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const navigate = useNavigate();
+  const user = useUser();
+  const [anchorElement, setAnchorElement] = useState(null);
 
-	const handleOpenNavMenu = (event) => {
-		setAnchorElNav(event.currentTarget);
-	};
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
-	};
+  const handleOpenUserMenu = (event) => {
+    setAnchorElement(event.currentTarget);
+  };
 
-	const handleCloseNavMenu = () => {
-		setAnchorElNav(null);
-	};
+  const handleCloseUserMenu = () => {
+    setAnchorElement(null);
+  };
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
-
-	return (
-		<AppBar position="static">
-			<Container maxWidth="xl">
-				<Toolbar disableGutters>
-					<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-					<Typography
-						variant="h6"
-						noWrap
-						component="a"
-						href="/"
-						sx={{
-							mr: 2,
-							display: { xs: 'none', md: 'flex' },
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							letterSpacing: '.3rem',
-							color: 'inherit',
-							textDecoration: 'none',
-						}}
-					>
-						LOGO
-					</Typography>
-
-					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-						<IconButton
-							size="large"
-							aria-label="account of current user"
-							aria-controls="menu-appbar"
-							aria-haspopup="true"
-							onClick={handleOpenNavMenu}
-							color="inherit"
-						>
-							<MenuIcon />
-						</IconButton>
-						<Menu
-							id="menu-appbar"
-							anchorEl={anchorElNav}
-							anchorOrigin={{
-								vertical: 'bottom',
-								horizontal: 'left',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'left',
-							}}
-							open={Boolean(anchorElNav)}
-							onClose={handleCloseNavMenu}
-							sx={{
-								display: { xs: 'block', md: 'none' },
-							}}
-						>
-							{pages.map((page) => (
-								<MenuItem key={page} onClick={handleCloseNavMenu}>
-									<Typography textAlign="center">{page}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
-					<AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-					<Typography
-						variant="h5"
-						noWrap
-						component="a"
-						href="/"
-						sx={{
-							mr: 2,
-							display: { xs: 'flex', md: 'none' },
-							flexGrow: 1,
-							fontFamily: 'monospace',
-							fontWeight: 700,
-							letterSpacing: '.3rem',
-							color: 'inherit',
-							textDecoration: 'none',
-						}}
-					>
-						LOGO
-					</Typography>
-					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-						{pages.map((page) => (
-							<Button
-								key={page}
-								onClick={handleCloseNavMenu}
-								sx={{ my: 2, color: 'white', display: 'block' }}
-							>
-								{page}
-							</Button>
-						))}
-					</Box>
-					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title="Open settings">
-							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-								<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: '45px' }}
-							id="menu-appbar"
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: 'top',
-								horizontal: 'right',
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
-						>
-							{settings.map((setting) => (
-								<MenuItem key={setting} onClick={handleCloseUserMenu}>
-									<Typography textAlign="center">{setting}</Typography>
-								</MenuItem>
-							))}
-						</Menu>
-					</Box>
-				</Toolbar>
-			</Container>
-		</AppBar>
-	);
+  return (
+    <AppBar position="static">
+      <Toolbar disableGutters sx={{ paddingX: 1.5 }}>
+        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title={user.name}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt={user.name} src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Popover
+            open={Boolean(anchorElement)}
+            anchorEl={anchorElement}
+            onClose={handleCloseUserMenu}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Stack padding={3} spacing={1} alignItems="center">
+              <Avatar
+                sx={{ width: 54, height: 54 }}
+                alt={user.name}
+                src="/static/images/avatar/2.jpg"
+              />
+              <Typography variant="body1" align="center">
+                {user.name}
+              </Typography>
+              <Typography variant="body2" align="center" color="gray">
+                {user.email}
+              </Typography>
+            </Stack>
+            <Stack padding={3} spacing={1} alignItems="center">
+              <Button
+                variant="contained"
+                fullWidth
+                size="small"
+                onClick={() => navigate("/profile")}
+              >
+                Edit Profile
+              </Button>
+              <Button variant="contained" fullWidth size="small" color="error">
+                Sign Out
+              </Button>
+            </Stack>
+          </Popover>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
 export default NavBar;
