bcf8ab931b3506b6553fa7bd0c59192e7929738c
gowribhat <gowri.bhat@u.nus.edu>
2023-09-13 02:01:58 +0800

    Change questions table styling

--- frontend-service/src/components/Questions/QuestionsTable.js
+++ frontend-service/src/components/Questions/QuestionsTable.js
@@ -1,64 +1,117 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
+import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Box from "@mui/material/Box";
 
 const QuestionsTable = ({ filteredQuestions }) => {
+  const navigate = useNavigate();
+
   const getComplexityStyle = (complexity) => {
     const colorMap = {
-      easy: "#008000",
-      medium: "#FFA500",
-      hard: "#FF0000",
+      easy: "primary",
+      medium: "warning",
+      hard: "error",
     };
   
-    return {
-      color: colorMap[complexity] || "",
-      textTransform: "capitalize",
-    };
+    return colorMap[complexity] || ""
   };
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
   if (filteredQuestions.length === 0) {
     return <Typography>No matching results</Typography>;
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Difficulty</TableCell>
-            <TableCell>Title</TableCell>
-            <TableCell>Category</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {filteredQuestions.map((question) => (
-            <TableRow
-              key={question.question_id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell>
-                <span style={getComplexityStyle(question.complexity)}>
-                  {question.complexity}
-                </span>
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {question.title}
-              </TableCell>
-              <TableCell>
-                {question.categories.map((cat) => (
-                  <div key={cat}>{cat}</div>
-                ))}
-              </TableCell>
+    <div style={{ display: "flex", justifyContent: "center", height: "auto" }}>
+      <TableContainer component={Paper} style={{ width: "80%", marginTop: "20px", maxHeight: 700 }}>
+        <Table sx={{ minWidth: 650 }} stickyHeader aria-label="simple table" >
+          <TableHead>
+            <TableRow>
+            <StyledTableCell>Difficulty</StyledTableCell>
+            <StyledTableCell>Title</StyledTableCell>
+            <StyledTableCell>Category</StyledTableCell>
+            <StyledTableCell>Actions</StyledTableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {filteredQuestions.map((question) => (
+              <StyledTableRow
+                key={question.question_id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <StyledTableCell>
+                  <Chip
+                    color={getComplexityStyle(question.complexity)}
+                    style={{ textTransform: "capitalize" }}
+                    label={question.complexity}
+                  />
+                </StyledTableCell>
+                <StyledTableCell component="th" scope="row">
+                  {question.title}
+                </StyledTableCell>
+                <StyledTableCell>
+                  <Box display="flex" gap={1}>
+                    {question.categories.map((cat) => (
+                      <Chip
+                        key={cat}
+                        label={cat}
+                        variant="outlined"
+                      />
+                    ))}
+                  </Box>
+                </StyledTableCell>
+                <StyledTableCell>
+                  <Box display="flex" gap={1}>
+                      <IconButton
+                        color="primary"
+                        aria-label="delete"
+                        size="large"
+                        onClick={() => navigate(`/question/${question.question_id}`)}
+                      >
+                        <EditIcon fontSize="inherit"/>
+                      </IconButton>
+                      <IconButton color="error" aria-label="delete" size="large">
+                        <DeleteIcon fontSize="inherit"/>
+                      </IconButton>
+                  </Box>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
   );
 };
 
