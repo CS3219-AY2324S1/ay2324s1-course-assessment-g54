46cdd04df7363882ea900b5da7b945951a9891c3
Gerald Teo <geraldteo7@gmail.com>
2023-09-13 11:14:07 +0800

    Add EditProfileModal

--- frontend-service/src/components/NavBar.js
+++ frontend-service/src/components/NavBar.js
@@ -13,22 +13,62 @@ import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import Modal from '@mui/material/Modal';
+import Card from '@mui/material/Card';
+import TextField from '@mui/material/TextField';
 
-function NavBar() {
+const NavBar = () => {
   const navigate = useNavigate();
   const user = useUser();
   const [anchorElement, setAnchorElement] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userName, setUserName] = useState(user.name);
+  const [inputName, setInputName] = useState(user.name);
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElement(event.currentTarget);
-  };
+  const handleOpenUserMenu = (event) => setAnchorElement(event.currentTarget);
+  const handleCloseUserMenu = () => setAnchorElement(null);
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+  const handleUpdateName = (event) => {
+    event.preventDefault();
+    setUserName(inputName);
+    setIsModalOpen(false);
+  }
 
-  const handleCloseUserMenu = () => {
-    setAnchorElement(null);
-  };
+  const EditProfileModal = (
+    <Modal open={isModalOpen} onClose={handleCloseModal} sx={{ display: "grid", placeContent: "center" }}>
+      <Card sx={{minWidth:400, textAlign: 'center', p: 1, borderRadius: 1, px: 2, py: 2 }}>
+        <form onSubmit={handleUpdateName}>
+          <Stack spacing={3}>
+            <Stack direction="row" alignItems="center" justifyContent="space-between" >
+              <Typography variant="h4" sx={{ fontSize: '30px', fontWeight: 400 }}>Edit Profile</Typography>
+              <Avatar
+                sx={{ width: 70, height: 70 }}
+                alt="a"
+                src="/static/images/avatar/2.jpg"
+              />
+            </Stack>
+            <Stack direction="row" alignItems="center" >
+              <Typography variant="body1" sx={{ fontWeight: 600, mr: 2 }}>Name</Typography>
+              <TextField defaultValue={userName} sx={{ flex: '1 1 auto' }} variant="outlined" onChange={(e) => setInputName(e.target.value)} />
+            </Stack>
+            <Stack direction="row" alignItems="center" >
+              <Typography variant="body1" sx={{ fontWeight: 600, mr: '18px' }}>Email</Typography>
+              <TextField disabled defaultValue={user.email} sx={{ flex: '1 1 auto' }} variant="outlined" />
+            </Stack>
+          </Stack>
+          <Stack direction="row" justifyContent="flex-end" pt={4} spacing={2}>
+            <Button variant="outlined" onClick={handleCloseModal} sx={{minWidth: 100}}> Cancel </Button>
+            <Button variant="contained" type='submit' sx={{minWidth: 100}}> Save</Button>
+          </Stack>
+        </form>
+      </Card>
+    </Modal>
+  );
 
   return (
     <AppBar position="static">
+      {isModalOpen && EditProfileModal}
       <Toolbar disableGutters sx={{ paddingX: 1.5 }}>
         <Box sx={{ flexGrow: 1 }} />
         <Box sx={{ flexGrow: 0 }}>
@@ -64,7 +104,7 @@ function NavBar() {
                 variant="contained"
                 fullWidth
                 size="small"
-                onClick={() => navigate("/profile")}
+                onClick={handleOpenModal}
               >
                 Edit Profile
               </Button>
