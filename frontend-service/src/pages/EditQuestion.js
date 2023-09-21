6d944926e1a6f614d9ca370849100e359c050a56
Gowri Bhat <97337263+gowribhat@users.noreply.github.com>
2023-09-22 05:25:11 +0800

    Fix ui bugs on edit page (#58)

--- frontend-service/src/pages/EditQuestion.js
+++ frontend-service/src/pages/EditQuestion.js
@@ -1,26 +1,20 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import SimpleMde from "react-simplemde-editor";
 
-import "easymde/dist/easymde.min.css";
-
-import ChipArray from "../components/ChipArray";
 import NavBar from "../components/NavBar";
-import SaveBar from "../components/SaveBar";
-import Selector from "../components/Selector";
+import AcknowledgementToast from "../components/AcknowledgementToast";
 
 import { useUser } from "../contexts/UserContext";
+import QuestionForm from "../components/QuestionForm";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
 import IconButton from "@mui/material/IconButton";
-import LinearProgress from "@mui/material/LinearProgress";
 import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const EditQuestion = () => {
   const navigate = useNavigate();
@@ -28,15 +22,21 @@ const EditQuestion = () => {
   const user = useUser();
   const [isLoading, setIsLoading] = useState(true);
 
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
-  const [alertMessage, setAlertMessage] = useState("");
-  const [alertSeverity, setAlertSeverity] = useState("");
+  const [toastOpen, setToastOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastSeverity, setToastSeverity] = useState("info");
 
   const [title, setTitle] = useState("");
   const [complexity, setComplexity] = useState("");
   const [categories, setCategories] = useState([]);
   const [description, setDescription] = useState("");
 
+  const showToast = (message, severity) => {
+    setToastMessage(message);
+    setToastSeverity(severity);
+    setToastOpen(true);
+  };
+  
   useEffect(() => {
     if (!user.isMaintainer) navigate("/questions");
 
@@ -54,19 +54,25 @@ const EditQuestion = () => {
       setCategories(categories);
       setDescription(description);
       setIsLoading(false);
+
+      const successMessage = window.localStorage.getItem("successMessage");
+      if (successMessage) {
+        showToast(successMessage, "success")
+      }
+      window.localStorage.removeItem("successMessage");
     };
 
     getQuestion();
   }, [id, navigate, user.isMaintainer]);
 
-  if (isLoading) return <LinearProgress variant="indeterminate" />;
-
   const handleSave = async () => {
     try {
       if (title.trim() === "") {
-        setAlertMessage("The question must have a title.");
-        setAlertSeverity("error");
-        setIsAlertOpen(true);
+        showToast("Question title must be at least 3 characters.", "error")
+        return;
+      }
+      if (description.trim().length < 3) {
+        showToast("Question description must be at least 3 characters.", "error")
         return;
       }
       const url = `${process.env.REACT_APP_QUESTIONS_SERVICE_HOST}/questions/${id}`;
@@ -74,43 +80,37 @@ const EditQuestion = () => {
       const header = {
         headers: { Authorization: token },
       };
-      const response = await axios.put(url, {
-        title,
-        complexity,
-        categories,
-        description,
-      },  header
-      );
-      setAlertMessage(
-        response.status === 200
-          ? "The question has been saved."
-          : "There was an error when trying to save question."
-      );
-      setAlertSeverity(response.status === 200 ? "success" : "error");
-      setIsAlertOpen(true);
+
+      const params = {
+        title: title.trim(),
+        complexity: complexity,
+        categories: categories,
+        description: description.trim()
+      }
+      const response = await axios.put(url, params, header);
+
+      if (response.status === 200) {
+        showToast("Question saved successfully!", "success")
+      } else {
+        showToast("Unable to save the question. Please try again!", "error")
+      }
     } catch (error) {
       console.error(error);
-      setAlertMessage("There was an error when trying to save question.");
-      setAlertSeverity("error");
-      setIsAlertOpen(true);
+      showToast("Unable to save the question. Please try again!", "error")
     }
   };
+
+  if (isLoading) return <LinearProgress variant="indeterminate" />;
 
   return (
     <>
       <NavBar />
-      <SaveBar
-        isOpen={isAlertOpen}
-        messsage={alertMessage}
-        severity={alertSeverity}
-        onClose={() => setIsAlertOpen(false)}
-      />
       <Box height="calc(100vh - 64px)" width="100%" bgcolor="whitesmoke">
         <Box height="100%" display="flex">
           <Stack height="100%" width="100%" spacing={1} padding={1}>
             <Box display="flex">
               <Tooltip title="Back to questions" placement="top" arrow>
-                <IconButton onClick={() => navigate(-1)}>
+                <IconButton onClick={() => navigate(`/questions`)}>
                   <ArrowBackIcon />
                 </IconButton>
               </Tooltip>
@@ -121,61 +121,25 @@ const EditQuestion = () => {
                 </Button>
               </Tooltip>
             </Box>
-            <Stack height="calc(100% - 48px)" direction="row" spacing={1}>
-              <Box width="50%" height="100%">
-                <Stack width="100%" spacing={1}>
-                  <Card variant="outlined" sx={{ padding: 1 }}>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      label="Question Title"
-                      defaultValue={title}
-                      onChange={(e) => setTitle(e.target.value)}
-                    />
-                  </Card>
-                  <Card variant="outlined" sx={{ padding: 1 }}>
-                    <Selector
-                      id="question-complexity"
-                      label="Question Complexity"
-                      options={["easy", "medium", "hard"]}
-                      value={complexity}
-                      onChange={(event) => setComplexity(event.target.value)}
-                    />
-                  </Card>
-                  <Card variant="outlined" sx={{ padding: 1 }}>
-                    <ChipArray
-                      chips={categories}
-                      helperText="Press enter to add a new category..."
-                      label="Categories"
-                      onAddChip={(newCategory) =>
-                        setCategories((prevCategories) => [
-                          ...prevCategories,
-                          newCategory,
-                        ])
-                      }
-                      onDeleteChip={(deletedCategory) =>
-                        setCategories((prevCategories) =>
-                          prevCategories.filter(
-                            (category) => category !== deletedCategory
-                          )
-                        )
-                      }
-                    />
-                  </Card>
-                </Stack>
-              </Box>
-              <Box width="50%" height="100%">
-                <Card sx={{ height: "100%", width: "100%", overflow: "auto" }}>
-                  <SimpleMde
-                    value={description}
-                    onChange={(value) => setDescription(value)}
-                  />
-                </Card>
-              </Box>
-            </Stack>
+            <QuestionForm
+              title={title}
+              complexity={complexity}
+              categories={categories}
+              description={description}
+              onTitleChange={setTitle}
+              onComplexityChange={setComplexity}
+              onCategoriesChange={setCategories}
+              onDescriptionChange={setDescription}
+            />
           </Stack>
         </Box>
       </Box>
+      <AcknowledgementToast
+        open={toastOpen}
+        message={toastMessage}
+        onClose={() => setToastOpen(false)}
+        severity={toastSeverity}
+      />
     </>
   );
 };
--- frontend-service/src/pages/NewQuestion.js
+++ frontend-service/src/pages/NewQuestion.js
@@ -1,33 +1,27 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import SimpleMde from "react-simplemde-editor";
 
-import "easymde/dist/easymde.min.css";
-
-import ChipArray from "../components/ChipArray";
 import NavBar from "../components/NavBar";
-import SaveBar from "../components/SaveBar";
-import Selector from "../components/Selector";
+import AcknowledgementToast from "../components/AcknowledgementToast";
+import QuestionForm from "../components/QuestionForm";
 
 import { useUser } from "../contexts/UserContext";
 
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
 import IconButton from "@mui/material/IconButton";
 import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
 
 const NewQuestion = () => {
   const navigate = useNavigate();
   const user = useUser();
 
-  const [isAlertOpen, setIsAlertOpen] = useState(false);
-  const [alertMessage, setAlertMessage] = useState("");
-  const [alertSeverity, setAlertSeverity] = useState("");
+  const [toastOpen, setToastOpen] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastSeverity, setToastSeverity] = useState("info");
 
   const [title, setTitle] = useState("");
   const [complexity, setComplexity] = useState("easy");
@@ -38,12 +32,20 @@ const NewQuestion = () => {
     if (!user.isMaintainer) navigate("/questions");
   }, [navigate, user.isMaintainer]);
 
+  const showToast = (message, severity) => {
+    setToastMessage(message);
+    setToastSeverity(severity);
+    setToastOpen(true);
+  };
+
   const handleSave = async () => {
     try {
-      if (title.trim() === "") {
-        setAlertMessage("The question must have a title.");
-        setAlertSeverity("error");
-        setIsAlertOpen(true);
+      if (title.trim().length < 3) {
+        showToast("Question title must be at least 3 characters.", "error")
+        return;
+      }
+      if (description.trim().length < 3) {
+        showToast("Question description must be at least 3 characters.", "error")
         return;
       }
       const url = `${process.env.REACT_APP_QUESTIONS_SERVICE_HOST}/questions`;
@@ -51,40 +53,32 @@ const NewQuestion = () => {
       const header = {
         headers: { Authorization: token },
       };
-      const response = await axios.post(
-        url,
-        {
-          title,
-          complexity,
-          categories,
-          description,
-        },
-        header
-      );
-      setAlertMessage(
-        response.status === 200
-          ? "The question has been saved."
-          : "There was an error when trying to save question."
-      );
-      setAlertSeverity(response.status === 200 ? "success" : "error");
-      setIsAlertOpen(true);
+
+      const params = {
+        title: title.trim(),
+        complexity: complexity,
+        categories: categories,
+        description: description.trim()
+      }
+
+      const response = await axios.post(url, params, header);
+      const question_id = response.data.question_id
+
+      if (response.status === 200) {
+        window.localStorage.setItem("successMessage", "Question saved successfully!");
+        navigate(`/questions/${question_id}/edit`)
+      } else {
+        showToast("Unable to save the question. Please try again!", "error")
+      }
     } catch (error) {
       console.error(error);
-      setAlertMessage("There was an error when trying to save question.");
-      setAlertSeverity("error");
-      setIsAlertOpen(true);
+      showToast("Unable to save the question. Please try again!", "error")
     }
   };
 
   return (
     <>
       <NavBar />
-      <SaveBar
-        isOpen={isAlertOpen}
-        messsage={alertMessage}
-        severity={alertSeverity}
-        onClose={() => setIsAlertOpen(false)}
-      />
       <Box height="calc(100vh - 64px)" width="100%" bgcolor="whitesmoke">
         <Box height="100%" display="flex">
           <Stack height="100%" width="100%" spacing={1} padding={1}>
@@ -101,61 +95,25 @@ const NewQuestion = () => {
                 </Button>
               </Tooltip>
             </Box>
-            <Stack height="calc(100% - 48px)" direction="row" spacing={1}>
-              <Box width="50%" height="100%">
-                <Stack width="100%" spacing={1}>
-                  <Card variant="outlined" sx={{ padding: 1 }}>
-                    <TextField
-                      fullWidth
-                      variant="outlined"
-                      label="Question Title"
-                      defaultValue={title}
-                      onChange={(e) => setTitle(e.target.value)}
-                    />
-                  </Card>
-                  <Card variant="outlined" sx={{ padding: 1 }}>
-                    <Selector
-                      id="question-complexity"
-                      label="Question Complexity"
-                      options={["easy", "medium", "hard"]}
-                      value={complexity}
-                      onChange={(event) => setComplexity(event.target.value)}
-                    />
-                  </Card>
-                  <Card variant="outlined" sx={{ padding: 1 }}>
-                    <ChipArray
-                      chips={categories}
-                      helperText="Press enter to add a new category..."
-                      label="Categories"
-                      onAddChip={(newCategory) =>
-                        setCategories((prevCategories) => [
-                          ...prevCategories,
-                          newCategory,
-                        ])
-                      }
-                      onDeleteChip={(deletedCategory) =>
-                        setCategories((prevCategories) =>
-                          prevCategories.filter(
-                            (category) => category !== deletedCategory
-                          )
-                        )
-                      }
-                    />
-                  </Card>
-                </Stack>
-              </Box>
-              <Box width="50%" height="100%">
-                <Card sx={{ height: "100%", width: "100%", overflow: "auto" }}>
-                  <SimpleMde
-                    value={description}
-                    onChange={(value) => setDescription(value)}
-                  />
-                </Card>
-              </Box>
-            </Stack>
+            <QuestionForm
+              title={title}
+              complexity={complexity}
+              categories={categories}
+              description={description}
+              onTitleChange={setTitle}
+              onComplexityChange={setComplexity}
+              onCategoriesChange={setCategories}
+              onDescriptionChange={setDescription}
+            />
           </Stack>
         </Box>
       </Box>
+      <AcknowledgementToast
+        open={toastOpen}
+        message={toastMessage}
+        onClose={() => setToastOpen(false)}
+        severity={toastSeverity}
+      />
     </>
   );
 };
