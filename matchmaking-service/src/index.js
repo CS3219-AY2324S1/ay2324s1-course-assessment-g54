dbbb2555bf3adc25a3044768bbab570e5b7137a8
Bryan Loh <bryanlohxz@gmail.com>
2023-09-21 23:35:11 +0800

    Add authorization guard to matchmaking-service

--- matchmaking-service/src/index.js
+++ matchmaking-service/src/index.js
@@ -1,13 +1,31 @@
 import { WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({ port: process.env.PORT });
+const authenticationUrl = `${process.env.USERS_SERVICE_HOST}/profile`;
 
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+wss.on("connection", async (ws, request) => {
+  const token = request.headers.authorization;
+  if (!token) {
+    console.log("Token cannot be found in authorization header.");
+    return ws.close(1008, "Unauthorized");
+  }
 
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
+  let user = null;
+  try {
+    const authResponse = await fetch(authenticationUrl, {
+      method: "GET",
+      headers: { Authorization: token },
+    });
+    if (authResponse.status !== 200) throw new Error(authResponse.body);
+    user = await authResponse.json();
+    if (!user) {
+      console.log("Invalid token found in authorization header.");
+      return ws.close(1008, "Unauthorized");
+    }
+  } catch (error) {
+    console.error(error);
+    return ws.close(1011, "Internal Server Error");
+  }
 
-  ws.send("something");
+  return ws.close(1000);
 });
