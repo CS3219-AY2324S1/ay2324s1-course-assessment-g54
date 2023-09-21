cd15333a960b750dc7cbb6b977439394e2462ea8
Bryan Loh <bryanlohxz@gmail.com>
2023-09-22 04:46:52 +0800

    Refactor try catch block in matchmaking service

--- matchmaking-service/src/index.js
+++ matchmaking-service/src/index.js
@@ -10,48 +10,40 @@ const wss = new WebSocketServer({ port: process.env.PORT });
 const difficulties = ["easy", "medium", "hard"];
 
 wss.on("connection", async (ws, request) => {
-  const token = request.headers.authorization;
-  if (!token) {
-    console.log("Token cannot be found in authorization header.");
-    return ws.close(1008, "Unauthorized");
-  }
-
-  let user = null;
   try {
-    user = await getUserFromToken(token);
+    const token = request.headers.authorization;
+    if (!token) {
+      console.log("Token cannot be found in authorization header.");
+      return ws.close(1008, "Unauthorized");
+    }
+
+    let user = await getUserFromToken(token);
     if (!user) {
       console.log("Invalid token found in authorization header.");
       return ws.close(1008, "Unauthorized");
     }
-  } catch (error) {
-    console.error(error);
-    return ws.close(1011, "Internal Server Error");
-  }
 
-  const urlSearchParams = new URLSearchParams(request.url.substring(1));
-  const difficulty = urlSearchParams.get("difficulty");
-  if (!difficulty || !difficulties.includes(difficulty.toLowerCase())) {
-    console.log("Difficulty is missing from search params or is incorrect.");
-    return ws.close(1000);
-  }
+    const urlSearchParams = new URLSearchParams(request.url.substring(1));
+    const difficulty = urlSearchParams.get("difficulty");
+    if (!difficulty || !difficulties.includes(difficulty.toLowerCase())) {
+      console.log("Difficulty is missing from search params or is incorrect.");
+      return ws.close(1000);
+    }
 
-  const matchResponseHandler = async (response) => {
-    console.log(response);
-    ws.close(1000, "Match has been found!");
-    return true;
-  };
+    const matchResponseHandler = async (response) => {
+      console.log(response);
+      ws.close(1000, "Match has been found!");
+      return true;
+    };
 
-  try {
     await subscribeMatchResponse(user.id, matchResponseHandler);
     await enqueueFindMatch(user.id, difficulty.toLowerCase());
+
+    ws.on("close", async () => {
+      await unsubscribeMatchResponse(user.id);
+    });
   } catch (error) {
     console.error(error);
     return ws.close(1011, "Internal Server Error");
   }
-
-  ws.on("close", async () => {
-    await unsubscribeMatchResponse(user.id);
-  });
-
-  if (!difficulty) request.trailers;
 });
