e9aad0ae73efac7592f1b23809ceaaddacf3fa05
Gerald Teo <geraldteo7@gmail.com>
2023-09-23 19:59:52 +0800

    Remove unused NavBar imports

--- frontend-service/src/pages/EditQuestion.js
+++ frontend-service/src/pages/EditQuestion.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import NavBar from "../components/NavBar";
 import AcknowledgementToast from "../components/AcknowledgementToast";
 
 import { useUser } from "../contexts/UserContext";
