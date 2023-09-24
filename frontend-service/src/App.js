4cda6387f722f93bdf33b5238812215a1c7e1ff5
Gerald Teo <geraldteo7@gmail.com>
2023-09-25 01:12:55 +0800

    Swap odd and even row question colors

--- frontend-service/src/App.js
+++ frontend-service/src/App.js
@@ -32,8 +32,8 @@ const darkTheme = createTheme({
     navbarButtonHover : "rgba(66, 165, 245, 0.7)",
     questionRowHover : "rgba(66, 165, 245, 0.7)",
     questionTableHead: "rgba(28, 38, 62, 1)",
-    questionRowEven: "rgba(28, 38, 62, 1)",
-    questionRowOdd: "rgba(28, 38, 62, 0.8)",
+    questionRowEven: "rgba(28, 38, 62, 0.8)",
+    questionRowOdd: "rgba(28, 38, 62, 1)",
   },
 })
 
