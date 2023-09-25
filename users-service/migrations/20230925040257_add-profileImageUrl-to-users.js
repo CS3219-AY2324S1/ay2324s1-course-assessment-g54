0ff9a9a9ef71b018ccc194e2e785d0619b3b2a67
Gerald Teo <geraldteo7@gmail.com>
2023-09-25 12:11:25 +0800

    Create knex migration file for adding profileImageUrl to users

---
+++ users-service/migrations/20230925040257_add-profileImageUrl-to-users.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.alterTable("users", function (table) {
+    table.boolean("profileImageUrl").defaultTo("");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.alterTable("users", function (table) {
+    table.dropColumn("profileImageUrl");
+  });
+};
