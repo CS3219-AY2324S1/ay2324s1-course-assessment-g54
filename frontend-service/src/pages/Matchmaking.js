973df1bb46ccf4fd38b898cc955e8dbe5b93fb35
Bryan Loh <bryanlohxz@gmail.com>
2023-10-03 23:27:47 +0800

    Edit matchmaking page to make it prettier

--- frontend-service/src/pages/Matchmaking.js
+++ frontend-service/src/pages/Matchmaking.js
@@ -1,51 +1,79 @@
-import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Chip,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import ToggleButton from "@mui/material/ToggleButton";
-import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import Typography from "@mui/material/Typography";
+const difficulties = ["easy", "medium", "hard"];
+const getDifficultyChipColor = (difficulty) => {
+  switch (difficulty) {
+    case "easy":
+      return "success";
+    case "medium":
+      return "warning";
+    case "hard":
+      return "error";
+    default:
+      return "primary";
+  }
+};
 
 const Matchmaking = () => {
   const navigate = useNavigate();
 
-  const [difficulty, setDifficulty] = useState('easy');
-
-  const handleDifficultyChange = (event, newDifficulty) => {
-    if (newDifficulty !== null) {
-      setDifficulty(newDifficulty);
-    }
-  }
-
   return (
-    <Stack alignItems="center" gap={2} pt={2}>
-      <Stack direction="row" alignItems="center" gap={2}>
-        <Typography fontSize={20}>Difficulty: </Typography>
-        <ToggleButtonGroup value={difficulty} exclusive onChange={handleDifficultyChange}>
-          <ToggleButton value="easy" sx={{ width: '100px' }}>
-            <Typography color="success" backgroundColor="success">
-              Easy
-            </Typography>
-          </ToggleButton>
-          <ToggleButton value="medium" sx={{ width: '100px' }}>
-            <Typography color="success" backgroundColor="success">
-              Medium
-            </Typography>
-          </ToggleButton>
-          <ToggleButton value="hard" sx={{ width: '100px' }}>
-            <Typography color="success" backgroundColor="success">
-              Hard
-            </Typography>
-          </ToggleButton>
-        </ToggleButtonGroup>
+    <Box height="calc(100vh - 64px)" width="100vw">
+      <Stack
+        spacing={6}
+        height="100%"
+        width="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Stack
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          direction="row"
+          spacing={3}
+        >
+          {difficulties.map((difficulty) => (
+            <Card key={difficulty} sx={{ width: "200px" }}>
+              <CardActionArea
+                onClick={() =>
+                  navigate(`/matchmaking/find?difficulty=${difficulty}`)
+                }
+              >
+                <CardContent
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  <Chip
+                    label={difficulty}
+                    color={getDifficultyChipColor(difficulty)}
+                    sx={{ marginTop: "150px" }}
+                  />
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Stack>
+        <Typography variant="h5">
+          Please select the difficulty of the question you are finding a match
+          for...
+        </Typography>
       </Stack>
-      <Button variant="contained" onClick={() => navigate(`/matchmaking/find?difficulty=${difficulty}`)}>
-        Let's Match!
-      </Button>
-    </Stack>
+    </Box>
   );
-}
+};
 
 export default Matchmaking;
