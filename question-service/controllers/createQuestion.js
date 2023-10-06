a50a92b56c03cfe2acbd8acd9a4511b8bae97537
Bryan Loh <bryanlohxz@gmail.com>
2023-10-06 15:25:45 +0800

    Add check for question title

--- question-service/controllers/createQuestion.js
+++ question-service/controllers/createQuestion.js
@@ -3,20 +3,27 @@ import { filterQuestion } from "./common.js";
 import { getNextQuestionId } from "./questionCounter.js";
 
 export async function createQuestion(req, res) {
-    try {
-        const createdQuestion = await populateDatabase(req.body);
-        return res.send(filterQuestion(createdQuestion));
-    } catch (error) {
-        return res.status(400).send(error.message);
-    }
+  try {
+    const existingQuestionsWithTitle = await Question.find({
+      title: req.body.title,
+    });
+    if (existingQuestionsWithTitle.length > 0)
+      return res
+        .status(400)
+        .send("Another question with the same title already exists.");
+    const createdQuestion = await populateDatabase(req.body);
+    return res.send(filterQuestion(createdQuestion));
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
 }
 
 export async function populateDatabase(q) {
-    const id = await getNextQuestionId();
-    const newQ = new Question({
-        question_id: id,
-        ...q
-    });
-    await newQ.save();
-    return newQ;
+  const id = await getNextQuestionId();
+  const newQ = new Question({
+    question_id: id,
+    ...q,
+  });
+  await newQ.save();
+  return newQ;
 }
