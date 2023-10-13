9e0997bafd79972449c1aa035a82b94bb96c8ba6
yew <e0726230@u.nus.edu>
2023-10-13 15:12:58 +0800

    Change tab icon

--- frontend-service/src/components/Page.js
+++ frontend-service/src/components/Page.js
@@ -6,7 +6,7 @@ const Page = (props) => {
   return <>
     <Helmet>
       <meta charSet="utf-8" />
-      <title>{`${title} | PeerPrep`}</title>
+      <title>{`${title} | OmegaLOLCode`}</title>
     </Helmet>
     {children}
   </>
