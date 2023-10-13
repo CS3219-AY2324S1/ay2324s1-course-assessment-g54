06c9b57a7e4d6f09e6ad2854edb4909c22e5afa9
Gerald Teo <geraldteo7@gmail.com>
2023-10-13 12:47:02 +0800

    Make image smaller

--- frontend-service/src/pages/Error.js
+++ frontend-service/src/pages/Error.js
@@ -11,13 +11,17 @@ const Example = () => {
       <Typography textAlign="center" variant="h3" fontWeight="bold">
         PAGE NOT FOUND
       </Typography>
+      <img
+        src={"PageNotFound.svg"}
+        alt="Page not found"
+        style={{ maxWidth: "100%" }}
+      />
       <Button 
         onClick={() => navigate(-1)}
         sx={{
           backgroundColor: (theme) => theme.palette.navbarButtonSelected, 
           fontSize: 20,
           py:2,
-          mt:4,
           color: "white",
           "&:hover": {
             backgroundColor: (theme) => theme.palette.navbarButtonHover,
@@ -26,11 +30,6 @@ const Example = () => {
       >
         Go back
       </Button>
-      <img
-        src={"PageNotFound.svg"}
-        alt="Page not found"
-        style={{ maxWidth: "100%" }}
-      />
     </Box>
   );
 };
