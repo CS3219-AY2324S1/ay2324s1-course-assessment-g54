b6f67a4b6a3ea803c6548c4e7b81f046e4da7223
Gerald Teo <geraldteo7@gmail.com>
2023-10-13 12:40:26 +0800

    Create go back button

--- frontend-service/src/pages/Error.js
+++ frontend-service/src/pages/Error.js
@@ -1,13 +1,31 @@
-import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 
 const Example = () => {
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ my: 4, display: "grid", placeContent: "center" }}>
       <Typography textAlign="center" variant="h3" fontWeight="bold">
         PAGE NOT FOUND
       </Typography>
+      <Button 
+        onClick={() => navigate("/questions")}
+        sx={{
+          backgroundColor: (theme) => theme.palette.navbarButtonSelected, 
+          fontSize: 20,
+          py:2,
+          mt:4,
+          color: "white",
+          "&:hover": {
+            backgroundColor: (theme) => theme.palette.navbarButtonHover,
+          },
+        }} 
+      >
+        Go back
+      </Button>
       <img
         src={"PageNotFound.svg"}
         alt="Page not found"
