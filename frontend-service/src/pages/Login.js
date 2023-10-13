effbc4fc0208903c6918cdec01616b393d00e733
yew <e0726230@u.nus.edu>
2023-10-13 12:19:40 +0800

    Added useEffect import statements

--- frontend-service/src/pages/Login.js
+++ frontend-service/src/pages/Login.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Box from "@mui/material/Box";
--- frontend-service/src/pages/Matchmaking.js
+++ frontend-service/src/pages/Matchmaking.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Box from "@mui/material/Box";
