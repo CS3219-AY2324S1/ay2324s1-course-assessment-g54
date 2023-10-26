e3d9d68653b24627f9eece4cbd9127ec2c1e0e0b
Bryan Loh <bryanlohxz@gmail.com>
2023-10-26 17:57:46 +0800

    Fix production dependecies problem

--- install-deps-dev.sh
+++ install-deps-dev.sh
@@ -2,6 +2,6 @@ npm install -g yarn
 services=("frontend-service" "question-service" "users-service" "matchmaking-service" "collab-service")
 for service in "${services[@]}"; do
   echo "Installing dependencies for $service..." &&
-  cd "$service" && yarn install --frozen-lockfile &&
+  cd "$service" && yarn install --frozen-lockfile --production=false &&
   cd ..
 done
