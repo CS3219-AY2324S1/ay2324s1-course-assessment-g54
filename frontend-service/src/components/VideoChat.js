51cbce666384a841f4d69e52b9e9212914cb1a9a
wongyewjon <e0726230@u.nus.edu>
2023-10-27 00:37:18 +0800

    Create a room and add a video chat component

--- frontend-service/src/components/VideoChat.js
+++ frontend-service/src/components/VideoChat.js
@@ -1,33 +1,67 @@
-import React, { useEffect, useRef } from 'react';
-import io from 'socket.io-client';
-import Peer from 'peerjs';
+import React, { useEffect, useRef, useState } from "react";
+import { Peer } from "peerjs";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
 const VideoChat = () => {
-  const remoteVideoRef = useRef(null);
-  const myVideoRef = useRef(null);
-  const socket = io('/');
-  const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001',
-  });
-  const peers = {};
+  const [peer, setPeer] = useState(null);
+  const [peerId, setPeerId] = useState(null);
+  const [socket, setSocket] = useState(null);
+
+  const videoGridRef = useRef(null);
+
+  const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+      video.play();
+    });
+    videoGridRef.current.appendChild(video);
+  }
 
   useEffect(() => {
+    const peer = new Peer();
+    peer.on("open", (id) => {
+      setPeerId(id);
+    });
+    setPeer(peer);
+    setSocket(io("http://localhost:3004"))
+  }, []);
+
+  useEffect(() => {
+    if (!socket) return;
+    if (!peer || !peerId) return;
+
+    const userVideo = document.createElement("video");
+    userVideo.muted = true;
+    const matchedUserVideo = document.createElement("video");
+
     const startMediaDevices = async () => {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        addVideoStream(myVideoRef.current, stream);
+        addVideoStream(userVideo, stream);
 
-        myPeer.on('call', (call) => {
+        peer.on('call', (call) => {
           call.answer(stream);
-          
-          call.on('stream', (userVideoStream) => {
-            addVideoStream(remoteVideoRef.current, userVideoStream);
+
+          call.on('stream', (matchedUserVideoStream) => {
+            addVideoStream(matchedUserVideo, matchedUserVideoStream);
           });
+
+          call.on('close', () => {
+            matchedUserVideo.remove();
+          })
         });
 
-        socket.on('user-connected', (userId) => {
-          connectToNewUser(userId, stream);
+        socket.on('joinedRoom', (notif) => {
+          const { roomID, user } = notif;
+          const call = peer.call(user, stream);
+          call.on('stream', (matchedUserVideoStream) => {
+            addVideoStream(matchedUserVideo, matchedUserVideoStream);
+          });
+
+          call.on('close', () => {
+            userVideo.remove();
+          })
         });
       } catch (error) {
         console.error('Error accessing media devices.', error);
@@ -36,44 +70,18 @@ const VideoChat = () => {
 
     startMediaDevices();
 
-    socket.on('user-disconnected', (userId) => {
-      if (peers[userId]) {
-        peers[userId].close();
-      }
-    });
+    // socket.on('user-disconnected', (userId) => {
+    //   if (peers[userId]) {
+    //     peers[userId].close();
+    //   }
+    // });
 
-    myPeer.on('open', (id) => {
-      socket.emit('join-room', ROOM_ID, id);
-    });
+    // myPeer.on('open', (id) => {
+    //   socket.emit('join-room', ROOM_ID, id);
+    // });
   }, []);
 
-  function connectToNewUser(userId, stream) {
-    const call = myPeer.call(userId, stream);
-    const video = document.createElement('video');
-    call.on('stream', (userVideoStream) => {
-      addVideoStream(video, userVideoStream);
-    });
-    call.on('close', () => {
-      video.remove();
-    });
-
-    peers[userId] = call;
-  }
-
-  function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => {
-      video.play();
-    });
-    remoteVideoRef.current.appendChild(video);
-  }
-
-  return (
-    <div>
-      <video ref={remoteVideoRef} autoPlay playsInline></video>
-      <video ref={myVideoRef} muted autoPlay playsInline />
-    </div>
-  );
+  return <div id="videos" ref={videoGridRef}></div>;
 };
 
 export default VideoChat;
