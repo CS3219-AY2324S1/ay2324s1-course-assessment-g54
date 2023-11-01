9513e4f949c559becd976ae0dd0110218d1577f0
gowribhat <gowri.bhat@u.nus.edu>
2023-11-01 22:30:27 +0800

    Save token

--- tests/SignUp.test.js
+++ tests/SignUp.test.js
@@ -1,10 +1,37 @@
 const axios = require('axios');
 
-test('Test something that requires authentication', async () => {
-  const response = await axios.post('http://peerpreptest.bryanlohxz.com/api/users-service/signup', {
-    name: 'Test 3',
-    email: 'test3@example.com',
-    password: 'testpassword',
+const USERS_SERVICE_HOST = "http://peerpreptest.bryanlohxz.com/api/users-service"
+let token
+
+test('Sign up for new user profile', async () => {
+  const response = await axios.post(`${USERS_SERVICE_HOST}/signup`, {
+    name: 'Test User',
+    email: 'testuser@example.com',
+    password: 'testuser',
   });
-  expect(response.status).not.toBeNull();
+  
+  expect(response.status).toBe(200)
+  expect(response.data.token).not.toBeNull();
+  token = response.data.token
+  console.log(token)
 });
+
+test('Login into PeerPrepTest', async () => {
+  const response = await axios.post(`${USERS_SERVICE_HOST}/login`, {
+    email: 'testuser@example.com',
+    password: 'testuser',
+  });
+  expect(response.status).toBe(200);
+});
+
+// test('Delete user profile with a valid token', async () => {
+
+//   // Make a request to delete the user profile with the valid token
+//   const response = await axios.delete(`${USERS_SERVICE_HOST}/profile`, {
+//     headers: {
+//       Authorization: token,
+//     },
+//   });
+//   console.log(token)
+//   expect(response.status).toBe(200);
+// });
