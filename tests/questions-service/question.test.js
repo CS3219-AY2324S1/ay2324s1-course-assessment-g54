b6f7362c30f042cba8a910f96307cd116ae2689d
gowribhat <gowri.bhat@u.nus.edu>
2023-11-03 23:53:31 +0800

    Add test for adding question without permissions

--- tests/questions-service/question.test.js
+++ tests/questions-service/question.test.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 
+const { UNEXPECTED_SUCCESS_MSG } = require("../errors.js");
+
 const TEST_NAME = "Blake Boris";
 const TEST_EMAIL = "blake@example.com";
 const TEST_PWD = "blake123";
@@ -39,4 +41,28 @@ test('Get one questions', async () => {
   );
   expect(response.status).toBe(200);
   expect(response.data.question_id).toBe(1);
+});
+
+test('Add new questions without maintainer permission', async () => {
+  try {
+    const url = `${process.env.REACT_APP_QUESTIONS_SERVICE_HOST}/questions`;
+    const header = {
+      headers: { Authorization: token },
+    };
+    const params = {
+      title: "New Question",
+      complexity: "easy",
+      categories: [],
+      description: "testing question",
+    };
+
+    await axios.post(url, params, header);
+    
+    throw new Error(UNEXPECTED_SUCCESS_MSG);
+  } catch (error) {
+    expect(error.response.status).toBe(401)
+    expect(error.response.data).toBe("not a maintainer!")
+  }
+  
+  
 });
