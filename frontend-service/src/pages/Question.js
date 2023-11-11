cb111de2df3a7b501f9f1b0d4234999ef65f71e7
yew <e0726230@u.nus.edu>
2023-11-11 13:46:41 +0800

    Add custom syntax highlighting for python and java language in Question page

--- frontend-service/src/pages/Question.js
+++ frontend-service/src/pages/Question.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import * as DOMPurify from "dompurify";
 import * as marked from "marked";
 import Editor from "@monaco-editor/react";
+import * as monaco from 'monaco-editor';
 import { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -48,7 +49,7 @@ const Question = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [question, setQuestion] = useState(null);
   const [editorLanguage, setEditorLanguage] = useState("javascript");
- 
+
   useEffect(() => {
     const getQuestion = async () => {
       const url = `${process.env.REACT_APP_QUESTIONS_SERVICE_HOST}/questions/${id}`;
@@ -66,8 +67,126 @@ const Question = () => {
     getQuestion();
   }, [id, navigate]);
 
+  const pythonLanguageConfiguration = {
+    comments: {
+      lineComment: '#',
+    },
+    brackets: [['{', '}'], ['[', ']'], ['(', ')']],
+    autoClosingPairs: [
+      { open: '{', close: '}' },
+      { open: '[', close: ']' },
+      { open: '(', close: ')' },
+      { open: "'", close: "'" },
+      { open: '"', close: '"' },
+    ],
+    surroundingPairs: [
+      { open: '{', close: '}' },
+      { open: '[', close: ']' },
+      { open: '(', close: ')' },
+      { open: "'", close: "'" },
+      { open: '"', close: '"' },
+    ],
+  };
+
+  const pythonMonarchTokensProvider = {
+    tokenizer: {
+      root: [
+        [/(def|class|if|else|elif|while|for|try|in|except|finally|with|as|return|break|continue|pass|raise|import|from|as|global|nonlocal|assert|yield|lambda)\b/, 'keyword'],
+        [/\b(True|False|None)\b/, 'constant.language'],
+        [/\b(and|or|not)\b/, 'keyword.operator'],
+        [/\b(print|input|len|range|str|int|float|list|tuple|set|dict)\b/, 'support.function'],
+        [/\b([a-zA-Z_]\w*)\b/, 'identifier'],
+        [/\d+\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+        [/\b\d+\b/, 'number'],
+        [/'([^'\\]|\\.)*'/, 'string'],
+        [/"([^"\\]|\\.)*"/, 'string'],
+      ],
+    },
+  };
+  const pythonTheme = {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [
+      { token: 'keyword', foreground: '#569CD6', fontStyle: 'bold' },
+      { token: 'constant.language', foreground: '#B5CEA8' },
+      { token: 'keyword.operator', foreground: '#D4D4D4' },
+      { token: 'support.function', foreground: '#C586C0' },
+      { token: 'identifier', foreground: '#9CDCFE' },
+      { token: 'number.float', foreground: '#CE9178' },
+      { token: 'number', foreground: '#D7BA7D' },
+      { token: 'string', foreground: '#6A9955' },
+    ],
+    colors: {
+      'editor.background': '#1E1E1E',
+      'editor.foreground': '#D4D4D4',
+      'editorLineNumber.foreground': '#008800',
+      'editor.selectionBackground': '#DDF0FF22',
+    },
+  };
+
+
+  const javaLanguageConfiguration = {
+    comments: {
+      lineComment: '//',
+      blockComment: ['/*', '*/'],
+    },
+    brackets: [['{', '}'], ['[', ']'], ['(', ')']],
+    autoClosingPairs: [
+      { open: '{', close: '}' },
+      { open: '[', close: ']' },
+      { open: '(', close: ')' },
+      { open: "'", close: "'", notIn: ['string', 'comment'] },
+      { open: '"', close: '"', notIn: ['string', 'comment'] },
+    ],
+    surroundingPairs: [
+      { open: '{', close: '}' },
+      { open: '[', close: ']' },
+      { open: '(', close: ')' },
+      { open: "'", close: "'" },
+      { open: '"', close: '"' },
+    ],
+  };
+
+  const javaMonarchTokensProvider = {
+    tokenizer: {
+      root: [
+        [/\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue|default|do|double|else|enum|extends|final|finally|float|for|if|goto|int|interface|long|native|new|package|private|protected|public|return|short|static|strictfp|super|switch|synchronized|this|throw|throws|transient|try|void|volatile|while)\b/, 'keyword'],
+        [/\b(true|false|null)\b/, 'constant.language'],
+        [/\b(instanceof|new)\b/, 'keyword.operator'],
+        [/\b(System.out.print|System.out.println|String.valueOf|Math.abs|Math.max|Math.min|Arrays.sort)\b/, 'support.function'],
+        [/[a-zA-Z_]\w*/, 'identifier'],
+        [/\b\d+\b/, 'number'],
+        [/'([^'\\]|\\.)*'/, 'string'],
+        [/"([^"\\]|\\.)*"/, 'string'],
+      ],
+    },
+  };
+  
+  const javaTheme = {
+    base: 'vs-dark',
+    inherit: true,
+    rules: [
+      { token: 'keyword', foreground: '#569CD6', fontStyle: 'bold' },
+      { token: 'constant.language', foreground: '#B5CEA8' },
+      { token: 'keyword.operator', foreground: '#D4D4D4' },
+      { token: 'support.function', foreground: '#C586C0' },
+      { token: 'identifier', foreground: '#9CDCFE' },
+      { token: 'number', foreground: '#D7BA7D' },
+      { token: 'string', foreground: '#6A9955' },
+    ],
+    colors: {
+      'editor.background': '#1E1E1E',
+      'editor.foreground': '#D4D4D4',
+      'editorLineNumber.foreground': '#008800',
+      'editor.selectionBackground': '#DDF0FF22',
+    },
+  };
+
+
+
   useEffect(() => {
     editorRef.current?.focus();
+
   }, [editorLanguage]);
 
   const handleEditorLanguageChange = (event) => setEditorLanguage(event.target.value);
@@ -96,7 +215,7 @@ const Question = () => {
                 </Typography>
                 <Chip
                   label={question.complexity.charAt(0).toUpperCase() + question.complexity.substring(1)}
-                  color= {getDifficultyChipColor(question.complexity)}
+                  color={getDifficultyChipColor(question.complexity)}
                   size="small"
                   sx={{ color: "white" }}
                 />
@@ -144,7 +263,7 @@ const Question = () => {
                 <Select
                   value={editorLanguage}
                   onChange={handleEditorLanguageChange}
-                  sx={{height: 20, width: 130,m: 1}}
+                  sx={{ height: 20, width: 130, m: 1 }}
                 >
                   <MenuItem value="javascript">Javascript</MenuItem>
                   <MenuItem value="python">Python</MenuItem>
@@ -153,12 +272,21 @@ const Question = () => {
               </Stack>
               <Editor
                 language={editorLanguage}
-                value={editorLanguage === "python" ? "# Insert your code here\n" : "// Insert your code here\n" }
-                theme="vs-dark"
+                value={editorLanguage === "python" ? "# Insert your code here\n" : "// Insert your code here\n"}
+                theme={editorLanguage === "python" ? "python-theme" : "java-theme"}
+                beforeMount={(monaco) => {
+                  monaco.languages.setMonarchTokensProvider('python', pythonMonarchTokensProvider);
+                  monaco.languages.setLanguageConfiguration('python', pythonLanguageConfiguration);
+                  monaco.editor.defineTheme('python-theme', pythonTheme);
+                  monaco.languages.setMonarchTokensProvider('java', javaMonarchTokensProvider);
+                  monaco.languages.setLanguageConfiguration('java', javaLanguageConfiguration);
+                  monaco.editor.defineTheme('java-theme', javaTheme);
+                }}
                 onMount={(editor) => {
                   editorRef.current = editor;
                   editorRef.current.focus();
                 }}
+
               />
             </Paper>
           </Box>
