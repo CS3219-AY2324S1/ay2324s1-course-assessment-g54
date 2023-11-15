97b629f61295a6179981ae37b491d8d18a2551e6
Bryan Loh <bryanlohxz@gmail.com>
2023-11-15 16:14:41 +0800

    Add check for short password

--- users-service/src/errors.js
+++ users-service/src/errors.js
@@ -6,6 +6,8 @@ export const INVALID_JWT_ERROR_MSG =
 export const INVALID_REQUEST_BODY_ERROR_MESSAGE =
   "Please check your request body.";
 
+export const PASSWORD_TOO_SHORT = "Password is too short.";
+
 export const SERVER_ERROR_MSG = "Sorry, something went wrong with the server.";
 
 export const USER_NOT_FOUND_MSG = "Sorry, the user cannot be found.";
